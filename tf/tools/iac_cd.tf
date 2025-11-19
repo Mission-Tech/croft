807e2b8b5e6329c9fd2cc666cbb2f15a0017ea9d
Izaak Lauer <8404559+izaaklauer@users.noreply.github.com>
2025-11-19 08:08:22 -0500

    upgrading hoist

--- tf/tools/iac_cd.tf
+++ tf/tools/iac_cd.tf
@@ -14,7 +14,7 @@ module "hoist_github_ci" {
 
 module "hoist_iac_cd" {
   # source = "../../../hoist/tf/modules/iac_cd"
-  source = "github.com/Mission-Tech/hoist//tf/modules/iac_cd?ref=experimental/iac_cd/v0.0.48"
+  source = "github.com/Mission-Tech/hoist//tf/modules/iac_cd?ref=iac_cd/v0.0.10"
   app    = local.app
   org    = var.org
   repo   = local.repo
